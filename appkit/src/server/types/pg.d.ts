0d1f3005848ff3d2d654e73d0ce4a8af59b3c0e1
poojaroonwit <jaroonwit.pool@gmail.com>
2026-02-19 21:58:44 +0700

    implement

--- appkit/src/server/types/pg.d.ts
+++ appkit/src/server/types/pg.d.ts
@@ -1,1 +1,29 @@
-declare module 'pg';
+declare module 'pg' {
+    export interface PoolConfig {
+        host?: string;
+        port?: number;
+        database?: string;
+        user?: string;
+        password?: string;
+        max?: number;
+        min?: number;
+        idleTimeoutMillis?: number;
+        connectionTimeoutMillis?: number;
+        statement_timeout?: number;
+        keepAlive?: boolean;
+        keepAliveInitialDelayMillis?: number;
+        connectionString?: string;
+        [key: string]: any;
+    }
+
+    export class Pool {
+        constructor(config?: PoolConfig);
+        on(event: string, listener: (...args: any[]) => void): this;
+        connect(): Promise<any>;
+        query(text: string, params?: any[]): Promise<any>;
+        // Properties used in database.ts
+        totalCount: number;
+        idleCount: number;
+        waitingCount: number;
+    }
+}
