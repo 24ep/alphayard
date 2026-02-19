2fbc755cae8ace78cf891195ecfcd601cfb9415a
poojaroonwit <jaroonwit.pool@gmail.com>
2026-02-20 00:08:37 +0700

    implement

---
+++ appkit/src/server/types/express-validator.d.ts
@@ -0,0 +1,11 @@
+declare module 'express-validator' {
+  export const validationResult: any;
+  export const check: any;
+  export const body: any;
+  export const param: any;
+  export const query: any;
+  export const oneOf: any;
+  export const header: any;
+  export const matchedData: any;
+  // Add any other exports you might use here
+}
